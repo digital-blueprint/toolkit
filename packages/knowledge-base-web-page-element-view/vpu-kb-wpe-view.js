7957b31ac9350fcb391549de0babe23af71fb01b
Eugen Neuber <eugen.neuber@tugraz.at>
2019-07-31 15:31:04 +0200

    Hide KB at page load

--- packages/knowledge-base-web-page-element-view/vpu-kb-wpe-view.js
+++ packages/knowledge-base-web-page-element-view/vpu-kb-wpe-view.js
@@ -140,6 +140,9 @@ class VPUKnowledgeBaseWebPageElementView extends VPULitElement {
         return html`
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
             <style>
+                .kb {
+                    display: none;
+                }
             </style>
 
             <img src='${this.eyeOpen}' @click="${this.toggle}" alt="open/close">
