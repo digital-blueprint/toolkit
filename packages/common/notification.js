217bb517c51d76ef7dcd4a3b30dd0793d2b19668
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2019-08-07 11:03:07 +0200

    Add notification send function with alert fallback

---
+++ packages/common/notification.js
@@ -0,0 +1,32 @@
+/**
+ * Sends a notification via the event
+ *
+ * For type see: https://bulma.io/documentation/elements/notification/#colors
+ *
+ * example options:
+ *
+ * {
+ *   "summary": "Item deleted",
+ *   "body": "Item foo was deleted!",
+ *   "type": "info",
+ *   "timeout": 5,
+ * }
+ *
+ * @param options
+ */
+function send(options) {
+    const event = new CustomEvent("vpu-notification-send", {
+        bubbles: true,
+        cancelable: true,
+        detail: options,
+    });
+
+    const result = window.dispatchEvent(event);
+
+    // true means the event was not handled
+    if (result) {
+        alert((options.summary !== undefined && options.summary !== "" ? options.summary + ": " : "") + options.body)
+    }
+}
+
+export { send };
