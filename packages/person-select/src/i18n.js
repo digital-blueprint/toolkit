a205176fce7d4da50d0c167bf240db8eb5faeaca
Christoph Reiter <reiter.christoph@gmail.com>
2019-08-08 14:40:00 +0200

    Port to new i18next wrapper

--- packages/person-select/src/i18n.js
+++ packages/person-select/src/i18n.js
@@ -1,29 +1,6 @@
-import i18next from 'i18next';
+import {createInstance} from 'vpu-common/i18next.js';
 
 import de from './i18n/de/translation.json';
 import en from './i18n/en/translation.json';
 
-const i18n = i18next.createInstance();
-
-i18n.init({
-    lng: 'de',
-    fallbackLng: ['de'],
-    debug: false,
-    initImmediate: false, // Don't init async
-    resources: {
-        en: {translation: en},
-        de: {translation: de}
-    },
-});
-
-console.assert(i18n.isInitialized);
-
-function dateTimeFormat(date, options) {
-    return new Intl.DateTimeFormat(i18n.languages, options).format(date);
-}
-
-function numberFormat(number, options) {
-    return new Intl.NumberFormat(i18n.languages, options).format(number);
-}
-
-export {i18n, dateTimeFormat, numberFormat};
+export const i18n = createInstance({en: en, de: de}, 'de', 'en');
