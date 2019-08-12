aab3c1fd2b03dbdf978aa7af004d002b29501ff8
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2019-08-12 08:59:04 +0200

    Add function makeId

--- packages/common/utils.js
+++ packages/common/utils.js
@@ -91,5 +91,24 @@ export default {
         }
         customElements.define(name, constructor, options);
         return true;
+    },
+
+    /**
+     * Creates a random id
+     *
+     * taken from: https://stackoverflow.com/a/1349426/1581487
+     *
+     * @param length
+     * @returns {string}
+     */
+    makeId: (length) => {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        return result;
     }
 };
