d3096a331f0ee0ad6b39b948e2a2e1e8447512de
Christoph Reiter <reiter.christoph@gmail.com>
2019-08-13 10:11:38 +0200

    Update dependencies

--- packages/language-select/rollup.config.js
+++ packages/language-select/rollup.config.js
@@ -35,9 +35,8 @@ export default {
         (build !== 'local' && build !== 'test') ? terser() : false,
         copy({
             targets: [
-                'assets/index.html',
+                {src: 'assets/index.html', dest: 'dist'},
             ],
-            outputFolder: 'dist'
         }),
         (process.env.ROLLUP_WATCH === 'true') ? serve({contentBase: 'dist', host: '127.0.0.1', port: 8002}) : false
     ]
