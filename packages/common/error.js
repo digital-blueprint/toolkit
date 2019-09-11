7b8eed47bce663b00780f8c794bc1c8f5d1a4fd3
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2019-09-11 10:49:08 +0200

    Add some documentation

--- packages/common/error.js
+++ packages/common/error.js
@@ -3,9 +3,11 @@ import {i18n} from "./i18n";
 
 export const xhrError = (jqXHR, textStatus, errorThrown) => {
     if (textStatus !== "abort") {
+        // try to show hydra error text
         let body = jqXHR.responseJSON !== undefined && jqXHR.responseJSON["hydra:description"] !== undefined ?
             jqXHR.responseJSON["hydra:description"] : textStatus;
 
+        // if the server is not reachable at all
         if (jqXHR.status === 0) {
             body = i18n.t('error.connection-to-server-refused');
         }
