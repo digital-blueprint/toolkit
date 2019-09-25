678569c6e0692b9fae53d9c388458e493999705d
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2019-09-25 14:08:03 +0200

    Fix selector spacing

--- packages/person-profile/src/person-profile-demo.js
+++ packages/person-profile/src/person-profile-demo.js
@@ -82,9 +82,9 @@ class PersonProfileDemo extends VPULitElement {
                     <h1 class="title">Select-Profile-Demo</h1>
                 </div>
                 <div class="container">
-                    <div class="control">
-                        <vpu-person-select lang="${this.lang}" entry-point-url="${commonUtils.getAPiUrl()}"></vpu-person-select>
-                    </div>
+                    <vpu-person-select lang="${this.lang}" entry-point-url="${commonUtils.getAPiUrl()}"></vpu-person-select>
+                </div>
+                <div class="container">
                     <vpu-person-profile lang="${this.lang}" entry-point-url="${commonUtils.getAPiUrl()}" value="${this.selectedPerson}"></vpu-person-profile>
                 </div>
             </section>
