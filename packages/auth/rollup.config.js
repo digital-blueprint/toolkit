8028087f2eca131c5b1a67c0534dcacb94205e55
Eugen Neuber <eugen.neuber@tugraz.at>
2019-09-30 14:06:18 +0200

    Add official login button images (login+hover)
    TODO: use them

--- packages/auth/rollup.config.js
+++ packages/auth/rollup.config.js
@@ -51,6 +51,8 @@ export default {
             targets: [
                 {src: 'assets/index.html', dest:'dist'},
                 {src: 'assets/favicon.ico', dest:'dist'},
+                {src: 'assets/icon_key_hover_tugprod.png', dest:'dist/local/vpu-auth'},
+                {src: 'assets/icon_key_normal_tugprod.png', dest:'dist/local/vpu-auth'},
                 {src: 'node_modules/material-design-icons-svg/paths/*.json', dest: 'dist/local/vpu-common/icons'},
             ]
         }),
