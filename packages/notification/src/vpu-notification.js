c41df1297cece4b1617cb4d7806d3f0f0b02cdca
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2019-09-30 15:47:14 +0200

    Fix a covering problem

--- packages/notification/src/vpu-notification.js
+++ packages/notification/src/vpu-notification.js
@@ -77,7 +77,7 @@ class VPUNotification extends VPULitElement {
         return html`
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
             <style>
-                #notification {position: fixed; top: 0; max-width: 500px; margin: 1% auto; left: 0; right: 0; z-index: 1000;}
+                #notification {position: fixed; top: 0; max-width: 500px; margin: 0.75em auto; left: 0; right: 0; z-index: 1000; padding: 0;}
                 .notification h3 {font-weight: bold; margin-bottom: 3px;}
             </style>
 
