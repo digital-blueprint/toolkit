0287718b9f177f15b9a956f43dc0d2f39830b034
Christoph Reiter <reiter.christoph@gmail.com>
2019-10-08 11:43:53 +0200

    Add a base docker image for testing
    This way we have a central place for the CI image config and also testing
    is faster because not everythign has to be installed on each run.

---
+++ packages/common/.gitlab-ci/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+TAG="registry.gitlab.tugraz.at/vpu/webcomponents/common/main:v1"
+sudo docker build --tag "${TAG}" --file "Dockerfile" .
+echo "Now run: sudo docker push '$TAG'"
