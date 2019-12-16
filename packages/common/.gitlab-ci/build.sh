0944352e770af372ccd0e6e3d914d162c3bdfa81
Christoph Reiter <reiter.christoph@gmail.com>
2019-12-16 11:19:25 +0100

    Add deployer to the CI docker image

--- packages/common/.gitlab-ci/build.sh
+++ packages/common/.gitlab-ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-TAG="registry.gitlab.tugraz.at/vpu/webcomponents/common/main:v2"
+TAG="registry.gitlab.tugraz.at/vpu/webcomponents/common/main:v3"
 sudo docker build --tag "${TAG}" --file "Dockerfile" .
 echo "Now run: sudo docker push '$TAG'"
