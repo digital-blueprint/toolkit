ce0dcc81ba7aeab0a9e57201c07fae6d4dee0338
Christoph Reiter <reiter.christoph@gmail.com>
2020-01-02 13:56:14 +0100

    Use debian again for the docker CI image
    It has a new enough firefox now

--- packages/common/.gitlab-ci/build.sh
+++ packages/common/.gitlab-ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-TAG="registry.gitlab.tugraz.at/vpu/webcomponents/common/main:v3"
+TAG="registry.gitlab.tugraz.at/vpu/webcomponents/common/main:v4"
 sudo docker build --tag "${TAG}" --file "Dockerfile" .
 echo "Now run: sudo docker push '$TAG'"
