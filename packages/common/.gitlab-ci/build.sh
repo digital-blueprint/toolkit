1058fb86ab6d269468084cbefd5d02c3782bcddf
Christoph Reiter <reiter.christoph@gmail.com>
2020-04-06 17:53:56 +0200

    ci: set MOZ_FORCE_DISABLE_E10S to work around firefox not working under docker sometimes

--- packages/common/.gitlab-ci/build.sh
+++ packages/common/.gitlab-ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-TAG="registry.gitlab.tugraz.at/vpu/webcomponents/common/main:v4"
+TAG="registry.gitlab.tugraz.at/vpu/webcomponents/common/main:v5"
 sudo docker build --tag "${TAG}" --file "Dockerfile" .
 echo "Now run: sudo docker push '$TAG'"
