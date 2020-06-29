d235dd0025450abbe500282ef2f023ba47e634db
Christoph Reiter <reiter.christoph@gmail.com>
2020-06-29 11:11:43 +0200

    Update npm deps

--- packages/auth/rollup.config.js
+++ packages/auth/rollup.config.js
@@ -1,14 +1,13 @@
 import path from 'path';
 import glob from 'glob';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 import {terser} from "rollup-plugin-terser";
-import json from 'rollup-plugin-json';
+import json from '@rollup/plugin-json';
 import serve from 'rollup-plugin-serve';
 import consts from 'rollup-plugin-consts';
 import del from 'rollup-plugin-delete';
-import chai from 'chai';
 
 const pkg = require('./package.json');
 const build = (typeof process.env.BUILD !== 'undefined') ? process.env.BUILD : 'local';
@@ -50,9 +49,6 @@ export default {
         }),
         commonjs({
             include: 'node_modules/**',
-            namedExports: {
-                'chai': Object.keys(chai),
-            }
         }),
         json(),
         (build !== 'local' && build !== 'test') ? terser() : false,
