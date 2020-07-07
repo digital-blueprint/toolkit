1b2b4a149ad75c4db577a927cb578f0abb76f1c7
Christoph Reiter <reiter.christoph@gmail.com>
2020-07-07 12:58:49 +0200

    Update npm deps

--- packages/data-table-view/rollup.config.js
+++ packages/data-table-view/rollup.config.js
@@ -1,15 +1,14 @@
 import path from 'path';
 import glob from 'glob';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 import {terser} from "rollup-plugin-terser";
-import json from 'rollup-plugin-json';
+import json from '@rollup/plugin-json';
 import serve from 'rollup-plugin-serve';
-import urlPlugin from "rollup-plugin-url";
+import urlPlugin from "@rollup/plugin-url";
 import consts from 'rollup-plugin-consts';
 import del from 'rollup-plugin-delete';
-import chai from 'chai';
 
 const pkg = require('./package.json');
 const build = (typeof process.env.BUILD !== 'undefined') ? process.env.BUILD : 'local';
@@ -56,10 +55,7 @@ export default {
             }
         }),
         commonjs({
-            include: 'node_modules/**',
-            namedExports: {
-                'chai': Object.keys(chai),
-              }
+            include: 'node_modules/**'
         }),
         json(),
         urlPlugin({
