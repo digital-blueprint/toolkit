6b186172d32787144bfc5f53f4013a6629a7fe66
Christoph Reiter <reiter.christoph@gmail.com>
2020-07-14 15:48:27 +0200

    Adjust gitlab paths

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-TAG="registry.gitlab.tugraz.at/dbp/webcomponents/toolkit/main:v1"
+TAG="registry.gitlab.tugraz.at/dbp/web-components/toolkit/main:v1"
 sudo docker build --tag "${TAG}" --file "Dockerfile" .
 echo "Now run: sudo docker push '$TAG'"
