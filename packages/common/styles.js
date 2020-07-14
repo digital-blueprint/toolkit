92e68708755f34c932890f4ca81ee3a984d914c8
Tamara Steinwender <tamara.steinwender@tugraz.at>
2020-07-14 12:09:04 +0200

    Added common text Utilities

--- packages/common/styles.js
+++ packages/common/styles.js
@@ -867,3 +867,36 @@ export function getModalDialogCSS() {
         }
     `;
 }
+
+export function getTextUtilities() {
+    // language=css
+    return css`
+        .text-left {
+            text-align: left;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .text-right {
+            text-align: right;
+        }
+
+        .text-lowercase {
+            text-transform: lowercase;
+        }
+
+        .text-uppercase {
+            text-transform: uppercase;
+        }
+
+        .text-capitalize {
+            text-transform: capitalize;
+        }
+
+      
+    `;
+}
+
+
