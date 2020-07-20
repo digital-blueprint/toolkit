68a87639e207b61b896fc18c610e7f94d798b1ef
Christoph Reiter <reiter.christoph@gmail.com>
2020-07-20 15:42:52 +0200

    Use the rollup resolve plugin for finding package asssets
    Instead of hardcoding the paths of the node_modules directory we use the node-resolve
    plugin of rollup to find the root of the corresponding JS package and calcucate a path from
    there.
    
    Resolving a package requires calling an async function, so we have to use await in the rollup config.
    Luckily rollup supports configs wrapped in a promise, so we just have to wrap it in a function which returns
    a promise.

--- packages/app-shell/rollup.config.js
+++ packages/app-shell/rollup.config.js
@@ -6,71 +6,55 @@ import serve from 'rollup-plugin-serve';
 import consts from 'rollup-plugin-consts';
 import del from 'rollup-plugin-delete';
 import json from '@rollup/plugin-json';
+import {getBuildInfo, getPackagePath} from '../../rollup.utils.js';
 
 const build = (typeof process.env.BUILD !== 'undefined') ? process.env.BUILD : 'local';
 console.log("build: " + build);
 
-function getBuildInfo() {
-    const child_process = require('child_process');
-    const url = require('url');
-
-    let remote = child_process.execSync('git config --get remote.origin.url').toString().trim();
-    let commit = child_process.execSync('git rev-parse --short HEAD').toString().trim();
-
-    let parsed = url.parse(remote);
-    let newPath = parsed.path.slice(0, parsed.path.lastIndexOf('.'));
-    let newUrl = parsed.protocol + '//' + parsed.host + newPath + '/commit/' + commit;
-
+export default (async () => {
     return {
-        info: commit,
-        url: newUrl,
-        time: new Date().toISOString(),
-        env: build
+        input: (build !='test') ? ['src/dbp-app-shell.js', 'src/dbp-activity-example.js'] : glob.sync('test/**/*.js'),
+        output: {
+            dir: 'dist',
+            entryFileNames: '[name].js',
+            chunkFileNames: 'shared/[name].[hash].[format].js',
+            format: 'esm',
+            sourcemap: true
+          },
+        onwarn: function (warning, warn) {
+            // ignore chai warnings
+            if (warning.code === 'CIRCULAR_DEPENDENCY') {
+              return;
+            }
+            // keycloak bundled code uses eval
+            if (warning.code === 'EVAL') {
+                return;
+            }
+            warn(warning);
+        },
+        plugins: [
+            del({
+                targets: 'dist/*'
+              }),
+            consts({
+                environment: build,
+                buildinfo: getBuildInfo(build),
+            }),
+            resolve(),
+            commonjs(),
+            json(),
+            copy({
+                targets: [
+                    {src: 'assets/silent-check-sso.html', dest:'dist'},
+                    {src: 'assets/index.html', dest: 'dist'},
+                    {src: 'assets/*.json', dest: 'dist'},
+                    {src: await getPackagePath('dbp-common', 'assets/icons/*.svg'), dest: 'dist/local/dbp-common/icons'},
+                ],
+            }),
+            (process.env.ROLLUP_WATCH === 'true') ? serve({
+                contentBase: 'dist',
+                historyApiFallback: '/index.html',
+                host: '127.0.0.1', port: 8002}) : false
+        ]
     }
-}
-
-export default {
-    input: (build !='test') ? ['src/dbp-app-shell.js', 'src/dbp-activity-example.js'] : glob.sync('test/**/*.js'),
-    output: {
-        dir: 'dist',
-        entryFileNames: '[name].js',
-        chunkFileNames: 'shared/[name].[hash].[format].js',
-        format: 'esm',
-        sourcemap: true
-      },
-    onwarn: function (warning, warn) {
-        // ignore chai warnings
-        if (warning.code === 'CIRCULAR_DEPENDENCY') {
-          return;
-        }
-        // keycloak bundled code uses eval
-        if (warning.code === 'EVAL') {
-            return;
-        }
-        warn(warning);
-    },
-    plugins: [
-        del({
-            targets: 'dist/*'
-          }),
-        consts({
-            environment: build,
-            buildinfo: getBuildInfo(),
-        }),
-        resolve(),
-        commonjs(),
-        json(),
-        copy({
-            targets: [
-                {src: 'assets/silent-check-sso.html', dest:'dist'},
-                {src: 'assets/index.html', dest: 'dist'},
-                {src: 'assets/*.json', dest: 'dist'},
-                {src: 'node_modules/dbp-common/assets/icons/*.svg', dest: 'dist/local/dbp-common/icons'},
-            ],
-        }),
-        (process.env.ROLLUP_WATCH === 'true') ? serve({
-            contentBase: 'dist',
-            historyApiFallback: '/index.html',
-            host: '127.0.0.1', port: 8002}) : false
-    ]
-};
+})();
--- packages/data-table-view/rollup.config.js
+++ packages/data-table-view/rollup.config.js
@@ -8,70 +8,72 @@ import serve from 'rollup-plugin-serve';
 import urlPlugin from "@rollup/plugin-url";
 import consts from 'rollup-plugin-consts';
 import del from 'rollup-plugin-delete';
+import {getPackagePath} from '../../rollup.utils.js';
 
 const pkg = require('./package.json');
 const build = (typeof process.env.BUILD !== 'undefined') ? process.env.BUILD : 'local';
 console.log("build: " + build);
 
-export default {
-    input: (build != 'test') ? ['src/dbp-data-table-view.js', 'src/dbp-data-table-view-demo.js'] : glob.sync('test/**/*.js'),
-    output: {
-        dir: 'dist',
-        entryFileNames: '[name].js',
-        chunkFileNames: 'shared/[name].[hash].[format].js',
-        format: 'esm',
-        sourcemap: true
-    },
-    onwarn: function (warning, warn) {
-        // ignore "suggestions" warning re "use strict"
-        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-            return;
-        }
-        // ignore chai warnings
-        if (warning.code === 'CIRCULAR_DEPENDENCY') {
-            return;
-        }
-        // keycloak bundled code uses eval
-        if (warning.code === 'EVAL') {
-            return;
-        }
-        warn(warning);
-    },
-    watch: {
-        chokidar: true,
-    },
-    plugins: [
-        del({
-            targets: 'dist/*'
-        }),
-        consts({
-            environment: build,
-        }),
-        resolve(),
-        commonjs(),
-        json(),
-        urlPlugin({
-            limit: 0,
-            emitFiles: true,
-            fileName: 'shared/[name].[hash][extname]'
-          }),
-        (build !== 'local' && build !== 'test') ? terser() : false,
-        copy({
-            targets: [
-                {src: 'assets/index.html', dest: 'dist'}, //rename: pkg.name + '.html'},
-                {src: 'assets/*.css', dest: 'dist/local/' + pkg.name},
-                {src: 'assets/*.ico', dest: 'dist/local/' + pkg.name},
-                {src: 'node_modules/dbp-common/dbp-spinner.js', dest: 'dist/local/' + pkg.name, rename: 'spinner.js'},
-                {src: 'node_modules/dbp-common/assets/icons/*.svg', dest: 'dist/local/dbp-common/icons'},
-                {src: 'assets/nomodule.js', dest: 'dist/local/' + pkg.name},
+export default (async () => {
+    return {
+        input: (build != 'test') ? ['src/dbp-data-table-view.js', 'src/dbp-data-table-view-demo.js'] : glob.sync('test/**/*.js'),
+        output: {
+            dir: 'dist',
+            entryFileNames: '[name].js',
+            chunkFileNames: 'shared/[name].[hash].[format].js',
+            format: 'esm',
+            sourcemap: true
+        },
+        onwarn: function (warning, warn) {
+            // ignore "suggestions" warning re "use strict"
+            if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+                return;
+            }
+            // ignore chai warnings
+            if (warning.code === 'CIRCULAR_DEPENDENCY') {
+                return;
+            }
+            // keycloak bundled code uses eval
+            if (warning.code === 'EVAL') {
+                return;
+            }
+            warn(warning);
+        },
+        watch: {
+            chokidar: true,
+        },
+        plugins: [
+            del({
+                targets: 'dist/*'
+            }),
+            consts({
+                environment: build,
+            }),
+            resolve(),
+            commonjs(),
+            json(),
+            urlPlugin({
+                limit: 0,
+                emitFiles: true,
+                fileName: 'shared/[name].[hash][extname]'
+            }),
+            (build !== 'local' && build !== 'test') ? terser() : false,
+            copy({
+                targets: [
+                    {src: 'assets/index.html', dest: 'dist'},
+                    {src: 'assets/*.css', dest: 'dist/local/' + pkg.name},
+                    {src: 'assets/*.ico', dest: 'dist/local/' + pkg.name},
+                    {src: 'assets/nomodule.js', dest: 'dist/local/' + pkg.name},
 
-                {src: '../../node_modules/datatables.net-dt/css', dest: 'dist/local/dbp-data-table-view'},
-                {src: '../../node_modules/datatables.net-dt/images', dest: 'dist/local/dbp-data-table-view'},
-                {src: '../../node_modules/datatables.net-responsive-dt/css', dest: 'dist/local/dbp-data-table-view'},
-                {src: '../../node_modules/datatables.net-buttons-dt/css', dest: 'dist/local/dbp-data-table-view'},
-            ],
-        }),
+                    {src: await getPackagePath('dbp-common', 'assets/icons/*.svg'), dest: 'dist/local/dbp-common/icons'},
+                    {src: await getPackagePath('datatables.net-dt', 'css'), dest: 'dist/local/dbp-data-table-view'},
+                    {src: await getPackagePath('datatables.net-dt', 'images'), dest: 'dist/local/dbp-data-table-view'},
+                    {src: await getPackagePath('datatables.net-responsive-dt', 'css'), dest: 'dist/local/dbp-data-table-view'},
+                    {src: await getPackagePath('datatables.net-buttons-dt', 'css'), dest: 'dist/local/dbp-data-table-view'},
+                ],
+            }),
 
-        (process.env.ROLLUP_WATCH === 'true') ? serve({contentBase: 'dist', host: '127.0.0.1', port: 8003}) : false
-    ]
-};
+            (process.env.ROLLUP_WATCH === 'true') ? serve({contentBase: 'dist', host: '127.0.0.1', port: 8003}) : false
+        ]
+    };
+})();
