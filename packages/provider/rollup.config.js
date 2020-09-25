43e83a59be04c09e1e7c15e9a51fd0b2e33e8773
Eugen Neuber <eugen.neuber@tugraz.at>
2020-09-25 15:17:28 +0200

    Add a provider web component plus demo
    See issue #31

---
+++ packages/provider/rollup.config.js
@@ -0,0 +1,104 @@
+import glob from 'glob';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
+import {terser} from "rollup-plugin-terser";
+import json from '@rollup/plugin-json';
+import serve from 'rollup-plugin-serve';
+import url from "@rollup/plugin-url";
+import consts from 'rollup-plugin-consts';
+import del from 'rollup-plugin-delete';
+import emitEJS from 'rollup-plugin-emit-ejs'
+import {getPackagePath} from '../../rollup.utils.js';
+
+const pkg = require('./package.json');
+const build = (typeof process.env.BUILD !== 'undefined') ? process.env.BUILD : 'local';
+console.log("build: " + build);
+
+function getBuildInfo() {
+    const child_process = require('child_process');
+    const url = require('url');
+
+    let remote = child_process.execSync('git config --get remote.origin.url').toString().trim();
+    let commit = child_process.execSync('git rev-parse --short HEAD').toString().trim();
+
+    let parsed = url.parse(remote);
+    // convert git urls
+    if (parsed.protocol === null) {
+        parsed = url.parse('git://' + remote.replace(":", "/"));
+        parsed.protocol = 'https:';
+    }
+    let newPath = parsed.path.slice(0, parsed.path.lastIndexOf('.'));
+    let newUrl = parsed.protocol + '//' + parsed.host + newPath + '/commit/' + commit;
+
+    return {
+        info: commit,
+        url: newUrl,
+        time: new Date().toISOString(),
+        env: build
+    }
+}
+
+export default (async () => {
+    return {
+        input: (build != 'test') ? ['src/dbp-provider.js', 'src/dbp-provider-demo.js'] : glob.sync('test/**/*.js'),
+        output: {
+            dir: 'dist',
+            entryFileNames: '[name].js',
+            chunkFileNames: 'shared/[name].[hash].[format].js',
+            format: 'esm',
+            sourcemap: true
+        },
+        onwarn: function (warning, warn) {
+            // keycloak bundled code uses eval
+            if (warning.code === 'EVAL') {
+                return;
+            }
+            warn(warning);
+        },
+        plugins: [
+            del({
+                targets: 'dist/*'
+            }),
+            consts({
+                environment: build,
+                buildInfo: getBuildInfo(),
+            }),
+            emitEJS({
+                src: 'assets',
+                include: ['**/*.ejs', '**/.*.ejs'],
+                data: {
+                    getUrl: (p) => {
+                        return url.resolve(basePath, p);
+                    },
+                    getPrivateUrl: (p) => {
+                        return url.resolve(`${basePath}local/${pkg.name}/`, p);
+                    },
+                    name: pkg.name,
+                    environment: build,
+                    buildInfo: getBuildInfo()
+                }
+            }),
+            resolve(),
+            commonjs(),
+            url({
+                limit: 0,
+                include: [
+                    await getPackagePath('select2', '**/*.css'),
+                ],
+                emitFiles: true,
+                fileName: 'shared/[name].[hash][extname]'
+            }),
+            json(),
+            (build !== 'local' && build !== 'test') ? terser() : false,
+            copy({
+                targets: [
+                    {src: 'assets/index.html', dest: 'dist'},
+                    {src: await getPackagePath('dbp-common', 'assets/icons/*.svg'), dest: 'dist/local/dbp-common/icons'},
+                    {src: 'assets/favicon.ico', dest:'dist'},
+                ],
+            }),
+            (process.env.ROLLUP_WATCH === 'true') ? serve({contentBase: 'dist', host: '127.0.0.1', port: 8002}) : false
+        ]
+    };
+})();
---
+++ packages/provider/src/provider.js
@@ -0,0 +1,87 @@
+export class Provider extends HTMLElement {
+    constructor() {
+        super();
+        this.callbackStore = [];
+
+        console.log('Provider constructor()');
+    }
+
+    static get observedAttributes() {
+        return ['init'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log('Provider(' + this.id() + ') attribute "' + name + '" changed from "' + oldValue + '" to "' + newValue + '".');
+        switch(name) {
+            case 'init':
+                this.init = newValue;
+                if (newValue) {
+                    const attrs = newValue.split(',');
+                    attrs.forEach(element => {
+                        const pair = element.trim().split('=');
+                        const name = pair[0].trim();
+                        const value = pair[1].trim().replace('"', '').replace("'", '');
+                        if (name.length > 0) {
+                            this[name] = value;
+                            this.callbackStore.forEach(item => {
+                                if (item.name === name) {
+                                    item.callback(value);
+                                }
+                            });
+                        }
+                    });
+                }
+                break;
+            default:
+                console.log('unknown attribute "' + name + '".');
+        }
+    }
+
+
+    connectedCallback() {
+        console.log('Provider(' + this.id() + ') connectedCallback()');
+
+        const that = this;
+        const parent = this.parentElement;
+        parent.addEventListener('inherit', function (e) {
+            if (that[e.detail.name]) {
+                console.log('Provider(' + that.id() + ') eventListener("inherit",..) name "' + e.detail.name + '" found.');
+                //console.dir(e.detail);
+                e.detail.callback(that[e.detail.name]);
+                e.stopPropagation();
+            }
+        }, false);
+
+        parent.addEventListener('subscribe', function (e) {
+            const name = e.detail.name;
+            if (that[name]) {
+                console.log('Provider(' + that.id() + ') eventListener("subscribe",..) name "' + name + '" found.');
+                that.callbackStore.push({name: name, callback: e.detail.callback, sender: e.detail.sender});
+
+                e.detail.callback(that[name]);
+                e.stopPropagation();
+            }
+        }, false);
+
+        parent.addEventListener('unsubscribe', function (e) {
+            const name = e.detail.name;
+            const sender = e.detail.sender;
+            if (that[name]) {
+                console.log('Provider(' + that.id() + ') eventListener("unsubscribe",..) name "' + name + '" found.');
+                that.callbackStore.forEach(item => {
+                    if (item.sender === sender && item.name === name) {
+                        const index = that.callbackStore.indexOf(item);
+                        that.callbackStore.splice(index, 1);
+                        console.log('Provider(' + that.id() + ') eventListener for name "' + name + '" removed.')
+                    }
+                });
+
+                e.stopPropagation();
+            }
+        }, false);
+    }
+
+    id() {
+        return this.getAttribute('id');
+    }
+}
