d9608283b2abaf076875edb4e8a1339fb6eec1e6
Tamara Steinwender <tamara.steinwender@tugraz.at>
2020-11-17 15:15:56 +0100

    Update toolkit demo

---
+++ demo/src/dbp-data-table-view-demo-activity.js
@@ -0,0 +1,58 @@
+import {css, html, LitElement} from 'lit-element';
+import {ScopedElementsMixin} from '@open-wc/scoped-elements';
+import {DataTableViewDemo} from '@dbp-toolkit/data-table-view/src/dbp-data-table-view-demo';
+import * as commonUtils from '@dbp-toolkit/common/utils';
+import * as commonStyles from '@dbp-toolkit/common/styles';
+import {unsafeHTML} from 'lit-html/directives/unsafe-html.js';
+import readme from '@dbp-toolkit/data-table-view/README.md';
+import highlightCSSPath from 'highlight.js/styles/default.css';
+import * as demoStyles from "./styles";
+
+class DbpActivityNameDemoActivity extends ScopedElementsMixin(LitElement) { //TODO
+    static get scopedElements() {
+        return {
+            'dbp-data-table-view-demo': DataTableViewDemo,
+        };
+    }
+
+    static get properties() {
+        return {
+        };
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.updateComplete.then(()=>{
+        });
+    }
+
+    static get styles() {
+        // language=css
+        return [
+            commonStyles.getThemeCSS(),
+            commonStyles.getGeneralCSS(),
+            demoStyles.getDemoCSS(),
+            css`
+            h1.title {margin-bottom: 1em;}
+            div.container {margin-bottom: 1.5em;}
+
+            #demo{
+                display: block;
+                padding-top: 50px;
+            }
+            
+            `
+        ];
+    }
+
+    render() {
+        return html`
+
+                ${unsafeHTML(readme)}
+                <dbp-data-table-view-demo id="demo" lang="en"></dbp-data-table-view-demo>
+        `;
+    }
+}
+
+commonUtils.defineCustomElement('dbp-data-table-view-demo-activity', DbpActivityNameDemoActivity);
