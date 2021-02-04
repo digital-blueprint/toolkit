f05915d17d8b93634f1f875b6f8532536603beeb
Christina Toegl <toegl@tugraz.at>
2021-02-04 16:38:12 +0100

    Try to fix uncaught syntax errors in adapter-lit-element

--- packages/provider/src/adapter-lit-element.js
+++ packages/provider/src/adapter-lit-element.js
@@ -286,7 +286,11 @@ export class AdapterLitElement extends LitElement {
                 }
                 break;
             default:
-                super.attributeChangedCallback(name, oldValue, newValue);
+                // The function should not be called if newValue is empty but name and oldValue are set
+                // This should prevent 'Uncaught SyntaxError: JSON.parse unexpected end of data at line 1 column 1 of the JSON data'
+                if (newValue || !oldValue || !name) {
+                    super.attributeChangedCallback(name, oldValue, newValue);
+                }
         }
 
         // console.log("this.lang", this.tagName, name, this.lang);
