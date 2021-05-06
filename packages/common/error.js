1d9ea395f46e4a0c2b57bad942b1c68d852e6a3c
Patrizio Bekerle <patrizio@bekerle.com>
2021-05-06 09:16:13 +0200

    Add missing semicolon

--- packages/common/error.js
+++ packages/common/error.js
@@ -142,4 +142,4 @@ export const getStackTrace = () => {
     let stack = new Error().stack || '';
     stack = stack.split('\n').map(function (line) { return line.trim(); });
     return stack.splice(stack[0] === 'Error' ? 2 : 1);
-}
+};
