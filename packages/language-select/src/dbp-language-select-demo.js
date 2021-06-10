49986d8dade0bb09670cdbf3d4290e7803097119
Christoph Reiter <reiter.christoph@gmail.com>
2021-06-10 11:21:43 +0200

    language-select: fix the demo not changing languages
    It was missing a provider element

--- packages/language-select/src/dbp-language-select-demo.js
+++ packages/language-select/src/dbp-language-select-demo.js
@@ -3,6 +3,7 @@ import {LanguageSelect} from './language-select.js';
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import { ScopedElementsMixin } from '@open-wc/scoped-elements';
 import {AdapterLitElement} from "@dbp-toolkit/provider/src/adapter-lit-element";
+import {Provider} from "@dbp-toolkit/provider";
 
 
 class LanguageSelectDisplay extends AdapterLitElement {
@@ -33,21 +34,24 @@ export class LanguageSelectDemo extends ScopedElementsMixin(AdapterLitElement) {
         return {
           'dbp-language-select': LanguageSelect,
           'dbp-language-select-display': LanguageSelectDisplay,
+          'dbp-provider': Provider,
         };
       }
 
     render() {
         return html`
-            Select 1: <dbp-language-select subscribe="lang"></dbp-language-select>
-            <br>
-            <br>
-            Select 2: <dbp-language-select subscribe="lang"></dbp-language-select>
-            <br>
-            <br>
-            Current language 1: <dbp-language-select-display subscribe="lang"></dbp-language-select-display>
-            <br>
-            <br>
-            Current language 2: <dbp-language-select-display subscribe="lang"></dbp-language-select-display>
+            <dbp-provider lang="">
+                Select 1: <dbp-language-select subscribe="lang"></dbp-language-select>
+                <br>
+                <br>
+                Select 2: <dbp-language-select subscribe="lang"></dbp-language-select>
+                <br>
+                <br>
+                Current language 1: <dbp-language-select-display subscribe="lang"></dbp-language-select-display>
+                <br>
+                <br>
+                Current language 2: <dbp-language-select-display subscribe="lang"></dbp-language-select-display>
+            </dbp-provider>
         `;
     }
 }
