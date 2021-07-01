17dbff728b03634d9035394d20a9b9bb3c552e55
Christoph Reiter <reiter.christoph@gmail.com>
2021-07-01 17:43:19 +0200

    auth: fix default namespace for i18next-scanner

--- packages/auth/i18next-scanner.config.js
+++ packages/auth/i18next-scanner.config.js
@@ -8,7 +8,6 @@ module.exports = {
         removeUnusedKeys: true,
         func: {list: ['i18n.t', '_i18n.t']},
         lngs: ['en','de'],
-        defaultNs: 'dbp-auth',
         resource: {
             loadPath: 'src/i18n/{{lng}}/{{ns}}.json',
             savePath: 'src/i18n/{{lng}}/{{ns}}.json'
