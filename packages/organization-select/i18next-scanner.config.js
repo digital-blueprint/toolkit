cffb9d5ff0f81e696b0cf9fd9e36bcaae7254a6c
Christoph Reiter <reiter.christoph@gmail.com>
2021-07-01 17:40:25 +0200

    Register _i18n.t with i18next-scanner
    It only detects i18n.t by default

--- packages/organization-select/i18next-scanner.config.js
+++ packages/organization-select/i18next-scanner.config.js
@@ -6,6 +6,7 @@ module.exports = {
     options: {
         debug: false,
         removeUnusedKeys: true,
+        func: {list: ['i18n.t', '_i18n.t']},
         lngs: ['en','de'],
         resource: {
             loadPath: 'src/i18n/{{lng}}/{{ns}}.json',
