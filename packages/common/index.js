79804c11a35e0c60712c32c096ca81b6e0e22346
Patrizio Bekerle <patrizio@bekerle.com>
2021-07-08 09:24:43 +0200

    Import / export AdapterLitElement like the other exports

--- packages/common/index.js
+++ packages/common/index.js
@@ -6,6 +6,7 @@ import {Button, LoadingButton} from './src/button.js';
 import {Spinner} from './src/spinner.js';
 import {InlineNotification} from './src/inline-notification.js';
 import {Translated} from "./src/translated";
+import {AdapterLitElement} from './src/adapter-lit-element.js';
 
 export {EventBus, createLinkedAbortController, createTimeoutAbortSignal};
 export {getIconSVGURL, getIconCSS, Icon};
@@ -15,4 +16,4 @@ export {Spinner};
 export {InlineNotification};
 export {Translated};
 export * from './src/logger.js';
-export {AdapterLitElement} from './src/adapter-lit-element.js';
+export {AdapterLitElement};
