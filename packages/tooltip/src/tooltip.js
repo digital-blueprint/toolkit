ebd63b9b2658229b0016332cecd0bdb1165b63c4
Eugen Neuber <eugen.neuber@tugraz.at>
2021-09-08 16:28:19 +0200

    Change letter "i" to circle-info icon/svg

--- packages/tooltip/src/tooltip.js
+++ packages/tooltip/src/tooltip.js
@@ -45,8 +45,8 @@ export class TooltipElement extends ScopedElementsMixin(DBPLitElement) {
             ${commonStyles.getButtonCSS()}
 
             .info-icon {
-                color: red;
-                padding: 0px 4px;
+                /* color: red; TODO: css-var ? */
+                padding: 0 4px;
             }
 
         `;
@@ -62,8 +62,13 @@ export class TooltipElement extends ScopedElementsMixin(DBPLitElement) {
 
         return html`
             <link rel="stylesheet" href="${tippy2CSS}">
-            <div>
-                <dbp-icon name='information' class="info-icon" id="tooltip-icon"></dbp-icon>
+            <div class="info-icon">
+                <!-- https://icons.getbootstrap.com/icons/info-circle/ -->
+                <svg id="tooltip-icon"
+                     xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                </svg>
             </div>
             
         `;
