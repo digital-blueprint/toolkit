aa5ab6138c0f6a4493d4a774173f0be2477ad5e8
Christina Tögl <toegl@tugraz.at>
2021-09-14 16:08:57 +0200

    Add new information-circle icon and use it in info-tooltip component

--- packages/tooltip/src/info-tooltip.js
+++ packages/tooltip/src/info-tooltip.js
@@ -5,6 +5,7 @@ import * as commonStyles from '@dbp-toolkit/common/styles';
 import DBPLitElement from '@dbp-toolkit/common/dbp-lit-element';
 import tippy from 'tippy.js';
 import tippy2CSSPath from 'tippy.js/dist/tippy.css';
+import {Icon} from '@dbp-toolkit/common';
 
 export class InfoTooltip extends ScopedElementsMixin(DBPLitElement) {
 
@@ -13,6 +14,12 @@ export class InfoTooltip extends ScopedElementsMixin(DBPLitElement) {
         this.tippy = '';
         this.textContent = 'missing text.';
         this.interactive = false;
+    }
+
+    static get scopedElements() {
+        return {
+            'dbp-icon': Icon,
+        };
     }
 
     static get properties() {
@@ -44,9 +51,6 @@ export class InfoTooltip extends ScopedElementsMixin(DBPLitElement) {
 
             .info-icon {
                 display: inline;
-                opacity: 0.7;
-                top: 2px;
-                position: relative;
             }
 
             a {
@@ -67,12 +71,7 @@ export class InfoTooltip extends ScopedElementsMixin(DBPLitElement) {
         return html`
             <link rel="stylesheet" href="${tippy2CSS}">
             <div class="info-icon">
-                <!-- https://icons.getbootstrap.com/icons/info-circle/ -->
-                <svg id="info-tooltip-icon"
-                     xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                    <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                </svg>
+                <dbp-icon name="information-circle" id="info-tooltip-icon"></dbp-icon>
             </div>
             
         `;
