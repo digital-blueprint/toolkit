28f5ddeaea2e2d49e90a88bdbf059cb039b64167
Tamara Steinwender <tamara.steinwender@tugraz.at>
2021-11-09 12:24:02 +0100

    Add unit test for crypto

---
+++ packages/file-handling/test/crypto.js
@@ -0,0 +1,16 @@
+import {assert, expect} from '@esm-bundle/chai';
+import {encrypt, decrypt} from '../src/crypto';
+
+
+suite('encyptAndDecrypt', () => {
+    test('encrypt', async () => {
+        let res = await encrypt('token', 'my-payload');
+        assert.isString(res);
+    });
+    test('encryptAndDecrypt', async () => {
+        let payload = 'my-payload';
+        let secret = await encrypt('token', payload);
+        let plain = await decrypt('token', secret);
+        assert.equal(payload, plain);
+    });
+});
