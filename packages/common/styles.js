56067965ef768147cb3c9d59163d379b4decd3e4
Tamara Steinwender <tamara.steinwender@tugraz.at>
2022-02-02 08:51:54 +0100

    Adapt orange default style

--- packages/common/styles.js
+++ packages/common/styles.js
@@ -78,7 +78,7 @@ export function getThemeCSS() {
         --dbp-info: var(--dbp-override-info, var(--dbp-override-info-dark, var(--dbp-override-primary-bg-color, #2a4491)));
         --dbp-success: var(--dbp-override-success, var(--dbp-override-success-dark, var(--dbp-override-success-bg-color, #188018)));
         --dbp-warning-as-text: var(--dbp-override-warning-as-text, var(--dbp-override-warning-color, var(--dbp-override-warning-dark, #c15500)));
-        --dbp-warning: var(--dbp-override-warning, var(--dbp-override-warning-color, var(--dbp-override-warning-light, #f99a41)));
+        --dbp-warning: var(--dbp-override-warning, var(--dbp-override-warning-color, var(--dbp-override-warning-light, #ffad4d)));
         --dbp-danger: var(--dbp-override-danger, var(--dbp-override-danger-bg-color, var(--dbp-override-danger-dark, #de3535)));
         --dbp-border: var(--dbp-override-border, var(--dbp-override-border-dark, 1px solid #000000));
         --dbp-border-radius: var(--dbp-override-border-radius, 0px);
