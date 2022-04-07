1f10f9e003b61491df5a64076ebb677d4333659e
Christoph Reiter <reiter.christoph@gmail.com>
2022-04-07 15:53:47 +0200

    language-select: refresh translations

--- packages/language-select/i18next-scanner.config.js
+++ packages/language-select/i18next-scanner.config.js
@@ -5,12 +5,14 @@ module.exports = {
     output: './',
     options: {
         debug: false,
+        sort: true,
         removeUnusedKeys: true,
         func: {list: ['i18n.t', '_i18n.t']},
         lngs: ['en','de'],
         resource: {
             loadPath: 'src/i18n/{{lng}}/{{ns}}.json',
-            savePath: 'src/i18n/{{lng}}/{{ns}}.json'
+            savePath: 'src/i18n/{{lng}}/{{ns}}.json',
+            jsonIndent: 4,
         },
     },
 }
