759c9a24fe3863dd1c9d7658ce115d484c303d31
Christoph Reiter <reiter.christoph@gmail.com>
2022-08-10 11:23:31 +0200

    Add a common demo config for package demos
    Centralize the server config in one place, so it can easily be changed in one
    file for all package demos.
    
    This just adds it for the app-shell, for starters.

---
+++ demo.common.config.js
@@ -0,0 +1,7 @@
+export default {
+    basePath: '/dist/',
+    entryPointURL: 'http://127.0.0.1:8000',
+    keyCloakBaseURL: 'https://auth-dev.tugraz.at/auth',
+    keyCloakRealm: 'tugraz-vpu',
+    keyCloakClientId: 'auth-dev-mw-frontend-local',
+}
--- packages/app-shell/rollup.config.js
+++ packages/app-shell/rollup.config.js
@@ -1,20 +1,28 @@
 import glob from 'glob';
+import url from 'url';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 import serve from 'rollup-plugin-serve';
 import del from 'rollup-plugin-delete';
 import json from '@rollup/plugin-json';
+import emitEJS from 'rollup-plugin-emit-ejs';
 import {getPackagePath, getDistPath} from '../../rollup.utils.js';
+import config from '../../demo.common.config.js';
 
 const build = typeof process.env.BUILD !== 'undefined' ? process.env.BUILD : 'local';
 console.log('build: ' + build);
 
+const pkg = require('./package.json');
+const basePath = '/dist/';
+const appName = 'dbp-app-shell';
+
 export default (async () => {
+    let privatePath = await getDistPath(pkg.name);
     return {
         input:
             build != 'test'
-                ? ['src/dbp-app-shell.js', 'src/dbp-activity-example.js']
+                ? ['src/' + appName + '.js', 'src/dbp-activity-example.js']
                 : glob.sync('test/**/*.js'),
         output: {
             dir: 'dist',
@@ -27,13 +35,29 @@ export default (async () => {
             del({
                 targets: 'dist/*',
             }),
+            emitEJS({
+                src: 'assets',
+                include: ['**/*.ejs', '**/.*.ejs'],
+                data: {
+                    getUrl: (p) => {
+                        return url.resolve(basePath, p);
+                    },
+                    getPrivateUrl: (p) => {
+                        return url.resolve(`${basePath}${privatePath}/`, p);
+                    },
+                    name: appName,
+                    entryPointURL: config.entryPointURL,
+                    keyCloakBaseURL: config.keyCloakBaseURL,
+                    keyCloakRealm: config.keyCloakRealm,
+                    keyCloakClientId: config.keyCloakClientId,
+                },
+            }),
             resolve(),
             commonjs(),
             json(),
             copy({
                 targets: [
                     {src: 'assets/silent-check-sso.html', dest: 'dist'},
-                    {src: 'assets/index.html', dest: 'dist'},
                     {src: 'assets/*.json', dest: 'dist'},
                     {
                         src: await getPackagePath('@dbp-toolkit/common', 'assets/icons/*.svg'),
@@ -43,8 +67,8 @@ export default (async () => {
             }),
             process.env.ROLLUP_WATCH === 'true'
                 ? serve({
-                      contentBase: 'dist',
-                      historyApiFallback: '/index.html',
+                      contentBase: '.',
+                      historyApiFallback: basePath + 'index.html',
                       host: '127.0.0.1',
                       port: 8002,
                   })
