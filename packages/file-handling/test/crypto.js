25c3892adc7c99e5787c37049051309879023d80
Christoph Reiter <reiter.christoph@gmail.com>
2023-11-02 11:16:01 +0100

    file-handling: add a unit test for parseJwt()
    So we are sure it still works after upgrading jose

--- packages/file-handling/test/crypto.js
+++ packages/file-handling/test/crypto.js
@@ -1,5 +1,5 @@
 import {assert} from '@esm-bundle/chai';
-import {encrypt, decrypt} from '../src/crypto';
+import {encrypt, decrypt, parseJwt} from '../src/crypto';
 
 suite('encyptAndDecrypt', () => {
     test('encrypt', async () => {
@@ -11,5 +11,11 @@ suite('encyptAndDecrypt', () => {
         let secret = await encrypt('token', payload);
         let plain = await decrypt('token', secret);
         assert.equal(payload, plain);
+    });
+    test('parseJwt', async () => {
+        let parsed = parseJwt('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
+        assert.equal(parsed.sub, '1234567890');
+        assert.equal(parsed.name, 'John Doe');
+        assert.equal(parsed.iat, 1516239022);
     });
 });
