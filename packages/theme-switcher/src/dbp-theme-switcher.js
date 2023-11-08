8c428bc5c3c108b5f755f8896ee694a24b181519
Christoph Reiter <reiter.christoph@gmail.com>
2023-11-08 13:07:13 +0100

    Add a dbp-themed element which shows different content based on the theme
    <dbp-themed>
      <span slot="light">This is shown for a ligth theme</span>
      <span slot="dark">This is shown for a dark theme</span>
    </dbp-themed>

--- packages/theme-switcher/src/dbp-theme-switcher.js
+++ packages/theme-switcher/src/dbp-theme-switcher.js
@@ -1,4 +1,6 @@
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import {ThemeSwitcher} from './theme-switcher';
+import {Themed} from './themed';
 
 commonUtils.defineCustomElement('dbp-theme-switcher', ThemeSwitcher);
+commonUtils.defineCustomElement('dbp-themes', Themed);
