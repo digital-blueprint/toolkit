7c36418a486cd60b95c2519ed8b870d0eb9adfc6
zsuffa david <zsuffa.david@gmail.com>
2024-07-03 13:49:17 +0200

    add TypeScript definition files for dbp-lit-element

---
+++ packages/common/dbp-lit-element.d.ts
@@ -0,0 +1,19 @@
+import { AdapterLitElement } from './src/adapter-lit-element.js';
+
+export default class DBPLitElement extends AdapterLitElement {
+
+    /**
+     * Query selector within the shadow DOM or light DOM.
+     * @param selector - The selector to query.
+     * @returns The selected element or null if not found.
+     */
+    _(selector: string): HTMLElement | null;
+
+
+    /** Query selector within the shadow DOM or light DOM.
+    * @param selector - The selector to query.
+    * @returns The selected elements as a NodeList or null if not found.
+    */
+    _a(selector: string): NodeList | null;
+
+}
