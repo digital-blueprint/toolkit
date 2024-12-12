a52964029e206fc99673b34714ce4e41f553a761
Christoph Reiter <reiter.christoph@gmail.com>
2024-12-12 15:33:12 +0100

    Drop @dbp-toolkit/knowledge-base-web-page-element-view
    This never was finished and hasn't been touched in years.
    If needed we can always bring it back.

--- packages/knowledge-base-web-page-element-view/src/dbp-knowledge-base-web-page-element-view.js
+++
@@ -1,7 +0,0 @@
-import * as commonUtils from '@dbp-toolkit/common/utils';
-import {KnowledgeBaseWebPageElementView} from './knowledge-base-web-page-element-view.js';
-
-commonUtils.defineCustomElement(
-    'dbp-knowledge-base-web-page-element-view',
-    KnowledgeBaseWebPageElementView
-);
--- packages/knowledge-base-web-page-element-view/src/index.js
+++
@@ -1,3 +0,0 @@
-import {KnowledgeBaseWebPageElementView} from './knowledge-base-web-page-element-view.js';
-
-export {KnowledgeBaseWebPageElementView};
