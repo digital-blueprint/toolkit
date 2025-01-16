05c251e78bb5a1cf6cfeec565fdf0b45c1cb890e
Patrizio Bekerle <patrizio@bekerle.com>
2025-01-16 14:58:49 +0100

    form-elements: add one more test
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- packages/form-elements/test/unit.js
+++ packages/form-elements/test/unit.js
@@ -1,12 +1,31 @@
 import {assert} from 'chai';
 
-import '../src/elements/string.js';
+import '../src/elements/string';
+import '../src/demo.js';
 
 suite('dbp-form-elements basics', () => {
     let node;
 
     setup(async () => {
         node = document.createElement('dbp-string-element');
+        document.body.appendChild(node);
+        await node.updateComplete;
+    });
+
+    teardown(() => {
+        node.remove();
+    });
+
+    test('should render', () => {
+        assert.isNotNull(node.shadowRoot);
+    });
+});
+
+suite('dbp-form-elements-demo', () => {
+    let node;
+
+    setup(async () => {
+        node = document.createElement('dbp-form-elements-demo');
         document.body.appendChild(node);
         await node.updateComplete;
     });
