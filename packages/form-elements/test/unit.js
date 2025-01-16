b18a049c07f0d2ca85fac64320e3677e3ccbba4b
Patrizio Bekerle <patrizio@bekerle.com>
2025-01-16 15:21:38 +0100

    form-elements: try to enable dbp-form-elements basics test
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- packages/form-elements/test/unit.js
+++ packages/form-elements/test/unit.js
@@ -1,25 +1,25 @@
 import {assert} from 'chai';
 
-import '../src/elements/string';
-import '../src/demo.js';
+import '../src';
+import '../src/demo';
 
-// suite('dbp-form-elements basics', () => {
-//     let node;
-//
-//     setup(async () => {
-//         node = document.createElement('dbp-string-element');
-//         document.body.appendChild(node);
-//         await node.updateComplete;
-//     });
-//
-//     teardown(() => {
-//         node.remove();
-//     });
-//
-//     test('should render', () => {
-//         assert.isNotNull(node.shadowRoot);
-//     });
-// });
+suite('dbp-form-elements basics', () => {
+    let node;
+
+    setup(async () => {
+        node = document.createElement('dbp-string-element');
+        document.body.appendChild(node);
+        await node.updateComplete;
+    });
+
+    teardown(() => {
+        node.remove();
+    });
+
+    test('should render', () => {
+        assert.isNotNull(node.shadowRoot);
+    });
+});
 
 suite('dbp-form-elements-demo', () => {
     let node;
