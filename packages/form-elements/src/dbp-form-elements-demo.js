4cfbec361417e06de585196fade19a3d87d14261
Patrizio Bekerle <patrizio@bekerle.com>
2025-01-16 14:11:54 +0100

    form-elements: add demo component
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

---
+++ packages/form-elements/src/dbp-form-elements-demo.js
@@ -0,0 +1,163 @@
+import {createInstance} from './i18n.js';
+import {css, html} from 'lit';
+import {ScopedElementsMixin} from '@open-wc/scoped-elements';
+import * as commonUtils from '@dbp-toolkit/common/utils';
+import * as commonStyles from '@dbp-toolkit/common/styles';
+import DBPLitElement from '@dbp-toolkit/common/dbp-lit-element';
+import {DbpStringElement, DbpDateElement, DbpDateTimeElement, DbpEnumElement, DbpCheckboxElement} from './index.js';
+import {createRef, ref} from 'lit/directives/ref.js';
+
+export class FormElementsDemo extends ScopedElementsMixin(DBPLitElement) {
+    constructor() {
+        super();
+        this._i18n = createInstance();
+        this.lang = this._i18n.language;
+        this.mySpecialComponentStringRef = createRef();
+        this.myComponentDateTimeRef = createRef();
+        this.myComponentEnumRef = createRef();
+    }
+
+    static get scopedElements() {
+        return {
+            'dbp-string-element': DbpStringElement,
+            'dbp-date-element': DbpDateElement,
+            'dbp-datetime-element': DbpDateTimeElement,
+            'dbp-enum-element': DbpEnumElement,
+            'dbp-checkbox-element': DbpCheckboxElement,
+        };
+    }
+
+    static get properties() {
+        return {
+            ...super.properties,
+            lang: {type: String},
+        };
+    }
+
+    static get styles() {
+        // language=css
+        return [
+            commonStyles.getThemeCSS(),
+            commonStyles.getGeneralCSS(),
+            css`
+                h1.title {
+                    margin-bottom: 1em;
+                }
+                div.container {
+                    margin-bottom: 1.5em;
+                }
+            `,
+        ];
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.updateComplete.then(() => {
+            // Add a custom validation function to the special string component
+            this.mySpecialComponentStringRef.value.customValidationFnc = (value, evaluationData) => {
+                // If the value is empty, return an error message with the evaluation data
+                return value === '' ? ['evaluationData: ' + JSON.stringify(evaluationData)] : [];
+            };
+
+            // Add a custom validation function to the datetime component
+            this.myComponentDateTimeRef.value.customValidationFnc = (value) => {
+                const date = new Date(value);
+                return date < new Date() ? ['The date needs to be in the future'] : [];
+            };
+
+            // Set items for the enum component
+            this.myComponentEnumRef.value.setItems({item1: 'Item 1', item2: 'Item 2'});
+        });
+    }
+
+    update(changedProperties) {
+        if (changedProperties.has('lang')) {
+            this._i18n.changeLanguage(this.lang);
+        }
+        super.update(changedProperties);
+    }
+
+    render() {
+        const data = this.data || {};
+
+        return html`
+            <section class="section">
+                <div class="container">
+                    <h1 class="title">Form Elements Demo</h1>
+                </div>
+                ${this.getAuthComponentHtml()}
+                <div class="container">
+                    <form>
+                        <dbp-string-element
+                                subscribe="lang"
+                                name="myComponentString"
+                                label="My string"
+                                value=${data.myComponentString || ''}
+                                required>
+                        </dbp-string-element>
+
+                        <dbp-string-element
+                                subscribe="lang"
+                                name="myComponentLongString"
+                                label="My long string"
+                                value=${data.myComponentLongString || ''}
+                                rows="5"
+                                required>
+                        </dbp-string-element>
+
+                        <dbp-string-element
+                                ${ref(this.mySpecialComponentStringRef)}
+                                subscribe="lang"
+                                name="mySpecialComponentString"
+                                description="Shows the evaluation data in the error message if empty"
+                                label="My special string"
+                                value=${data.mySpecialComponentString || ''}
+                                required>
+                        </dbp-string-element>
+
+                        <dbp-date-element
+                                subscribe="lang"
+                                name="myComponentDate"
+                                label="My date"
+                                value=${data.myComponentDate || ''}
+                                required>
+                        </dbp-date-element>
+
+                        <dbp-datetime-element
+                                ${ref(this.myComponentDateTimeRef)}
+                                subscribe="lang"
+                                name="myComponentDateTime"
+                                description="Needs to be in the future"
+                                label="My datetime"
+                                value=${data.myComponentDateTime || ''}
+                                required>
+                        </dbp-datetime-element>
+
+                        <dbp-enum-element
+                                ${ref(this.myComponentEnumRef)}
+                                subscribe="lang"
+                                name="myComponentEnum"
+                                label="My enum"
+                                value=${data.myComponentEnum || ''}
+                                required>
+                        </dbp-enum-element>
+
+                        <dbp-checkbox-element
+                                subscribe="lang"
+                                name="myComponentCheckbox"
+                                label="My checkbox"
+                                description="Check me"
+                                value="check"
+                                ?checked=${data.myComponentCheckbox || false}>
+                        </dbp-checkbox-element>
+
+                        ${this.getButtonRowHtml()}
+                    </form>
+                </div>
+            </section>
+        `;
+    }
+}
+
+commonUtils.defineCustomElement('dbp-form-elements-demo', FormElementsDemo);
