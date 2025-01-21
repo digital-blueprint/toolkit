e3b91b32183f08adf07be55918449b87aeceb2e0
Patrizio Bekerle <patrizio@bekerle.com>
2025-01-21 14:06:28 +0100

    form-elements: add more tests
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- packages/form-elements/test/unit.js
+++ packages/form-elements/test/unit.js
@@ -3,7 +3,79 @@ import {assert} from 'chai';
 import '../src/index';
 import '../src/demo';
 
-suite('dbp-form-elements basics', () => {
+suite('dbp-checkbox-element', () => {
+    let node;
+
+    setup(async () => {
+        node = document.createElement('dbp-checkbox-element');
+        document.body.appendChild(node);
+        await node.updateComplete;
+    });
+
+    teardown(() => {
+        node.remove();
+    });
+
+    test('should render', () => {
+        assert.isNotNull(node.shadowRoot);
+    });
+});
+
+suite('dbp-date-element', () => {
+    let node;
+
+    setup(async () => {
+        node = document.createElement('dbp-date-element');
+        document.body.appendChild(node);
+        await node.updateComplete;
+    });
+
+    teardown(() => {
+        node.remove();
+    });
+
+    test('should render', () => {
+        assert.isNotNull(node.shadowRoot);
+    });
+});
+
+suite('dbp-datetime-element', () => {
+    let node;
+
+    setup(async () => {
+        node = document.createElement('dbp-datetime-element');
+        document.body.appendChild(node);
+        await node.updateComplete;
+    });
+
+    teardown(() => {
+        node.remove();
+    });
+
+    test('should render', () => {
+        assert.isNotNull(node.shadowRoot);
+    });
+});
+
+suite('dbp-enum-element', () => {
+    let node;
+
+    setup(async () => {
+        node = document.createElement('dbp-enum-element');
+        document.body.appendChild(node);
+        await node.updateComplete;
+    });
+
+    teardown(() => {
+        node.remove();
+    });
+
+    test('should render', () => {
+        assert.isNotNull(node.shadowRoot);
+    });
+});
+
+suite('dbp-string-element', () => {
     let node;
 
     setup(async () => {
