305954e87c2802c035b3efb7e7967939ca31a02b
Patrizio Bekerle <patrizio@bekerle.com>
2025-01-21 10:05:45 +0100

    nix: make karma tests working
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- shell.nix
+++ shell.nix
@@ -8,6 +8,14 @@
       lazygit # git terminal
       just # task runner
       fzf # fuzzy finder, for "just watch"
+      chromium # for karma tests
+      firefox # for karma tests
     ];
-}
 
+    shellHook = ''
+      export CHROMIUM_BIN=${pkgs.chromium}/bin/chromium
+      export FIREFOX_BIN=${pkgs.firefox}/bin/firefox
+
+      echo "Using chromium at $CHROMIUM_BIN and firefox at $FIREFOX_BIN for karma tests"
+    '';
+}
