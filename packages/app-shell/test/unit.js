712fb61fc6ea3a3f8de2742bc663c106775e9e28
Christoph Reiter <reiter.christoph@gmail.com>
2025-01-27 10:21:43 +0100

    router: make tests async and properly await results

--- packages/app-shell/test/unit.js
+++ packages/app-shell/test/unit.js
@@ -3,31 +3,42 @@ import {assert} from 'chai';
 import {Router} from '../src/router.js';
 
 suite('router', () => {
-    test('basics', () => {
+    test('basics', async () => {
         const routes = [
             {
                 name: 'foo',
                 path: '',
                 action: (context) => {
-                    return {};
+                    return {bar: false};
+                },
+            },
+            {
+                name: 'bar',
+                path: '/bar',
+                action: (context) => {
+                    return {bar: true};
                 },
             },
         ];
 
+        let myState = {};
         const router = new Router(routes, {
             routeName: 'foo',
             getState: () => {
-                return {};
+                return myState;
             },
-            setState: (state) => {},
+            setState: (state) => {
+                myState = state;
+            },
             getDefaultState: () => {
                 return {};
             },
         });
 
-        router.setStateFromCurrentLocation();
-        router.update();
-        router.updateFromUrl('/?foo=bar#quux');
+        await router.setStateFromCurrentLocation();
+        await router.update();
+        await router.updateFromUrl('/bar?foo=bar#quux');
+        assert.equal(myState.bar, true);
         assert.equal(router.getPathname(), '/');
     });
 });
