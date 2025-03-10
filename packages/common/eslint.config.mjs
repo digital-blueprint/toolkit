d9e186346a90d608ab07121f7f9f46b5c20163d7
Christoph Reiter <reiter.christoph@gmail.com>
2025-03-10 13:53:20 +0100

    Update to @open-wc/scoped-elements v3
    Since this requires the polyfill to be present, but we want to keep
    the old behaviour, we vendor the adapter from @lion/ui which allows
    us to update to v3 while keeping the polyfill optional and still provides
    methods that paper over polyfill/non-polyfill differences.

--- packages/common/eslint.config.mjs
+++ packages/common/eslint.config.mjs
@@ -2,7 +2,7 @@ import commonConfig from '../../eslint.common.config.mjs';
 
 export default [
     {
-        ignores: ['dist/', '*.cjs', 'src/micromodal.es.js'],
+        ignores: ['dist/', '*.cjs', 'src/micromodal.es.js', 'src/scoped/ScopedElementsMixin.js'],
     },
     ...commonConfig,
 ];
