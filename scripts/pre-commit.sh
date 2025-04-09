04a3aba567848f39b26d20ba1b981b54559bce43
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-09 14:49:50 +0200

    npm: refactor script naming and structure
    * "check": runs linters, formatters, translation linters and reports the status
    * "fix": tries to adjust the code to make "check" pass, if possbile
    
    For the other commands we just name them after the tools:
    
    * "tool": runs the tool but only reports changes/issues
    * "tool:fix": runs the tool and changes the code
    
    To summarize: "format" is now "fix" and also runs i18next fixes
    
    Also fix some missing commands in "language-select" while at it.
    And remove duplicate "lint" commands.

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -4,8 +4,8 @@
 echo "Running pre-commit hook with npm run check"
 
 # Check if eslint and prettier are happy with the code
-# We can't just use `npm run format` and `git diff --check --exit-code`, because git will also complain about not yet staged changes
+# We can't just use `npm run fix` and `git diff --check --exit-code`, because git will also complain about not yet staged changes
 if ! npm run check; then
-  echo -e "\nEslint or Prettier complained about the code. Please fix the issues and commit again. Often running 'npm run format' will fix the issues."
+  echo -e "\nEslint or Prettier complained about the code. Please fix the issues and commit again. Often running 'npm run fix' will fix the issues."
   exit 1
 fi
