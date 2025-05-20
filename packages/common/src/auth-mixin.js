deff376d9691c7ce5fa39eefcb90b58cbdefbc80
Christoph Reiter <reiter.christoph@gmail.com>
2025-05-20 15:40:11 +0200

    auth-mixin: add a isAuthPending() method
    In many cases we only want to show an error message if the user is not logged in,
    but only once we are sure. This method allows to query if the login state has settled yet.

--- packages/common/src/auth-mixin.js
+++ packages/common/src/auth-mixin.js
@@ -12,6 +12,7 @@ export const AuthMixin = dedupeMixin(
                 super();
                 this.auth = null;
                 this._previousAuthState = null;
+                this._authPending = true;
             }
 
             update(changedProps) {
@@ -25,14 +26,24 @@ export const AuthMixin = dedupeMixin(
                     const isLoggedOut = currentAuth['login-status'] === 'logged-out';
 
                     if (!wasLoggedIn && isLoggedIn) {
+                        this._authPending = false;
                         this.loginCallback(currentAuth);
                     }
                     if (!wasLoggedOut && isLoggedOut) {
+                        this._authPending = false;
                         this.logoutCallback();
                     }
                     this._previousAuthState = {...currentAuth};
                 }
                 super.update(changedProps);
+            }
+
+            /**
+             * @returns {boolean} - True if the auth state is not settled yet, i.e.
+             * loginCallback or logoutCallback has not been called yet.
+             */
+            isAuthPending() {
+                return this._authPending;
             }
 
             /**
