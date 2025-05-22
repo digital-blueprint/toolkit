8f9f4ac5c29c98ea3ef15cd4f3ec016296ae287c
Christoph Reiter <reiter.christoph@gmail.com>
2025-05-22 17:21:09 +0200

    language-select: remove provider usage
    it's not strictly needed here, and it's the only remaining user of
    the standalone provider component

--- packages/language-select/src/dbp-language-select-demo.js
+++ packages/language-select/src/dbp-language-select-demo.js
@@ -3,7 +3,6 @@ import {LanguageSelect} from './language-select.js';
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import {ScopedElementsMixin} from '@dbp-toolkit/common';
 import {AdapterLitElement} from '@dbp-toolkit/common';
-import {Provider} from '@dbp-toolkit/provider';
 import {createInstance, setOverrides} from './i18n.js';
 
 // This is an example on how to override translations at runtime
@@ -54,34 +53,32 @@ class LanguageSelectDisplay extends AdapterLitElement {
 export class LanguageSelectDemo extends ScopedElementsMixin(AdapterLitElement) {
     constructor() {
         super();
+        this.setAttribute('lang', '');
     }
 
     static get scopedElements() {
         return {
             'dbp-language-select': LanguageSelect,
             'dbp-language-select-display': LanguageSelectDisplay,
-            'dbp-provider': Provider,
         };
     }
 
     render() {
         return html`
-            <dbp-provider lang="">
-                Select 1:
-                <dbp-language-select subscribe="lang"></dbp-language-select>
-                <br />
-                <br />
-                Select 2:
-                <dbp-language-select subscribe="lang"></dbp-language-select>
-                <br />
-                <br />
-                Current language 1:
-                <dbp-language-select-display subscribe="lang"></dbp-language-select-display>
-                <br />
-                <br />
-                Current language 2:
-                <dbp-language-select-display subscribe="lang"></dbp-language-select-display>
-            </dbp-provider>
+            Select 1:
+            <dbp-language-select subscribe="lang"></dbp-language-select>
+            <br />
+            <br />
+            Select 2:
+            <dbp-language-select subscribe="lang"></dbp-language-select>
+            <br />
+            <br />
+            Current language 1:
+            <dbp-language-select-display subscribe="lang"></dbp-language-select-display>
+            <br />
+            <br />
+            Current language 2:
+            <dbp-language-select-display subscribe="lang"></dbp-language-select-display>
         `;
     }
 }
