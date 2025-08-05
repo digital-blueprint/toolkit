c59b65b8a7f2a17b98f8ec1d99418e6c4893752d
Christoph Reiter <reiter.christoph@gmail.com>
2025-08-05 14:51:20 +0200

    @babel/preset-env: move to newer baseline
    this drops safari 13 for example

--- toolkit-showcase/rollup.config.js
+++ toolkit-showcase/rollup.config.js
@@ -259,6 +259,10 @@ Dependencies:
             useBabel &&
                 getBabelOutputPlugin({
                     compact: false,
+                    targets: {
+                        esmodules: true,
+                        browsers: 'defaults and not dead, last 5 years',
+                    },
                     presets: [
                         [
                             '@babel/preset-env',
@@ -267,9 +271,6 @@ Dependencies:
                                 shippedProposals: true,
                                 bugfixes: true,
                                 modules: false,
-                                targets: {
-                                    esmodules: true,
-                                },
                             },
                         ],
                     ],
