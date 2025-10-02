3d90ab180570ad5c65faecf0bed425d6a35bf6fc
Christoph Reiter <reiter.christoph@gmail.com>
2025-10-02 16:55:17 +0200

    Port from i18next-parser to i18next-cli
    i18next-parser is deprecated and users should move to
    i18next-cli.
    
    Port the config to match the old one + adjust the npm commands
    to do the same thing. The error message when translations have
    changed is a bit confusing (when running "npm run i18next"),
    but we can't change that.

--- packages/common/i18next-parser.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    createOldCatalogs: false,
-    indentation: 4,
-    keepRemoved: false,
-    locales: ['en', 'de'],
-    output: 'src/i18n/$LOCALE/$NAMESPACE.json',
-    input: ['src/**/*.js', './*.js'],
-    sort: true,
-};
