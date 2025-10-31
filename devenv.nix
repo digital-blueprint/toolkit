8f7be3e79cc7f18e976c1dd751c6cf754e917eaf
Patrizio Bekerle <patrizio@bekerle.com>
2025-10-31 10:05:12 +0100

    devenv: use lerna in git hook
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- devenv.nix
+++ devenv.nix
@@ -3,4 +3,26 @@
   enterShell = ''
     echo "🛠️ DBP Toolkit Dev Shell"
   '';
+
+  # https://devenv.sh/git-hooks/
+  git-hooks = {
+    hooks = {
+      # We need to do those with lerna
+      eslint.enable = false;
+      i18next.enable = false;
+
+      # Our npm packages have different configs, so we need to use lerna to check
+      lerna-check = {
+        enable = true;
+        name = "lerna-check";
+        description = "Run check with lerna";
+        entry = "npm run check";
+        language = "system";
+        pass_filenames = false;
+        files = "(\\.js$|\\.json$)";
+        stages = [ "pre-commit" ];
+        require_serial = true;
+      };
+    };
+  };
 }
