ec30e703f6611ce8d3182c8aa32ae36cc16ef42c
Christoph Reiter <reiter.christoph@gmail.com>
2025-11-06 11:47:31 +0100

    sendNotification: improve docs
    various options were missing

--- packages/common/src/notification.js
+++ packages/common/src/notification.js
@@ -1,7 +1,5 @@
 /**
- * Sends a notification via the event
- *
- * Type can be info/success/warning/danger
+ * Sends a notification to the user.
  *
  * example options:
  *
@@ -9,11 +7,18 @@
  *   "summary": "Item deleted",
  *   "body": "Item foo was deleted!",
  *   "type": "info",
- *   "icon": "download",
+ *   "icon": "remove-file",
  *   "timeout": 5,
  * }
  *
- * @param options
+ * @param {object} options - Notification options
+ * @param {string} options.body - The main notification message (required)
+ * @param {string} [options.summary] - The notification title/summary
+ * @param {('info'|'success'|'warning'|'danger')} [options.type] - The notification type
+ * @param {string} [options.icon] - Icon name to display with the notification (if the handler supports it)
+ * @param {number} [options.timeout] - Duration in seconds before auto-dismissing the notification (if the handler supports it)
+ * @param {string} [options.targetNotificationId] - ID of specific notification component to target (if the handler supports it)
+ * @param {string} [options.replaceId] - Unique identifier to replace existing notifications with the same replaceId (if the handler supports it)
  */
 function sendNotification(options) {
     const event = new CustomEvent('dbp-notification-send', {
