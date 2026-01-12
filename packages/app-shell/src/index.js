57cd8bf6cb83bbaaad1ebac804a26cdc4ae3b11e
Christoph Reiter <reiter.christoph@gmail.com>
2026-01-12 15:13:41 +0100

    Remove the legacy app-shell
    The new one seems good now, so no need to easily compare things.

--- packages/app-shell/src/index.js
+++ packages/app-shell/src/index.js
@@ -1,8 +1,3 @@
-import {AppShell as AppShellNew} from './app-shell.js';
-import {AppShell as AppShellLegacy} from './app-shell-legacy.js';
-import {getFeatureFlag, registerFeatureFlag} from '@dbp-toolkit/common';
-
-registerFeatureFlag('app-shell-old');
-const AppShell = !getFeatureFlag('app-shell-old') ? AppShellNew : AppShellLegacy;
+import {AppShell} from './app-shell.js';
 
 export {AppShell};
