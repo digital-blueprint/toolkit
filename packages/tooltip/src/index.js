8568cfc53e94033834125d3d4b944bb050bacf8f
zsuffa david <zsuffa.david@gmail.com>
2026-01-16 14:59:27 +0100

    tooltip: add missing export for ButtonTooltip

--- packages/tooltip/src/index.js
+++ packages/tooltip/src/index.js
@@ -1,5 +1,7 @@
 import {InfoTooltip} from './info-tooltip.js';
 import {TooltipElement} from './tooltip.js';
+import {ButtonTooltip} from './button-tooltip.js';
 
 export {InfoTooltip as InfoTooltip};
 export {TooltipElement as TooltipElement};
+export {ButtonTooltip as ButtonTooltip};
