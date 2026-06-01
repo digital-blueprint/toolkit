7f2d80ef85ff73f9076798ee2de4ddc061e9a8b6
Andrea Ortner <andrea.ortner@tugraz.at>
2026-06-01 11:59:49 +0200

    align modal-footer with buttons to the bottom
    Signed-off-by: Andrea Ortner <andrea.ortner@tugraz.at>

--- packages/common/src/styles.js
+++ packages/common/src/styles.js
@@ -1548,6 +1548,9 @@ export function getNativeModalDialogCSS() {
             max-height: 100%;
             min-height: 100%;
             height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
         }
 
         :host([full-height-line]) .modal-content {
