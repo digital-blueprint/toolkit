cac2c452a269c94cbb33fc9b7a44a4af078b2375
Christoph Reiter <reiter.christoph@gmail.com>
2026-06-17 13:51:31 +0200

    Revert "resource-select: add override option for the placeholder text"
    This reverts commit 74306548f4fb671baaba62a3e5fe4dd36fba01f0.

--- packages/resource-select/test/unit.js
+++ packages/resource-select/test/unit.js
@@ -35,33 +35,6 @@ suite('dbp-resource-select basics', () => {
         assert.deepEqual(node.getSearchQueryParameters(node, ''), {});
         assert.deepEqual(node.getItemParameters(node), {});
     });
-
-    test('should use mode-specific placeholders', async () => {
-        node.auth = {token: 'test'};
-        node.lang = 'en';
-        await node.updateComplete;
-
-        assert.strictEqual(node._getPlaceholder(), 'Please select an entry');
-
-        node.fetchMode = 'search';
-        await node.updateComplete;
-
-        assert.strictEqual(node._getPlaceholder(), 'Please search for an entry');
-    });
-
-    test('should allow overriding the placeholder', async () => {
-        node.auth = {token: 'test'};
-        node.fetchMode = 'search';
-        node.formatPlaceholder = (select) => {
-            assert.strictEqual(select, node);
-            assert.strictEqual(select.fetchMode, 'search');
-
-            return 'Please search for a person';
-        };
-        await node.updateComplete;
-
-        assert.strictEqual(node._getPlaceholder(), 'Please search for a person');
-    });
 });
 
 suite('dbp-resource-select-demo basics', () => {
