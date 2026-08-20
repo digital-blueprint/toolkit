25a9d1ecb460ee2463d4a38d92c710bf925dcc81
Christoph Reiter <reiter.christoph@gmail.com>
2026-08-20 10:22:24 +0200

    common: add typescript-eslint

--- packages/common/eslint.config.mjs
+++ packages/common/eslint.config.mjs
@@ -1,8 +1,20 @@
 import commonConfig from '../../eslint.common.config.mjs';
+import tseslint from 'typescript-eslint';
 
 export default [
     {
-        ignores: ['dist/', '*.cjs', 'src/scoped/ScopedElementsMixin.js'],
+        ignores: ['dist/', '*.js', 'src/scoped/ScopedElementsMixin.js', 'misc/browser-check.js', 'test/'],
     },
+    ...tseslint.configs.recommended,
     ...commonConfig,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            }
+        },
+        rules:  {
+            '@typescript-eslint/no-unused-vars': ['error', {args: 'none'}],
+        }
+    },
 ];
