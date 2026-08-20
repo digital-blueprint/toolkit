e3f50ee33f7ee7d021715827e72529283df28717
Christoph Reiter <reiter.christoph@gmail.com>
2026-08-20 14:30:58 +0200

    eslint: add notes why the remaining rules are disabled

--- eslint-typescript.common.config.mjs
+++ eslint-typescript.common.config.mjs
@@ -9,12 +9,16 @@ export default [
             }
         },
         rules:  {
+            // We don't type everything, so disable those
             '@typescript-eslint/no-unsafe-argument': 'off',
             '@typescript-eslint/no-unsafe-assignment': 'off',
             '@typescript-eslint/no-unsafe-call': 'off',
             '@typescript-eslint/no-unsafe-member-access': 'off',
             '@typescript-eslint/no-unsafe-return': 'off',
+            // We keep unused vars for function args as documentation,
             '@typescript-eslint/no-unused-vars': ['error', {args: 'none'}],
+            // lit auto-binds methods in templates, which causes lots of false positives
+            // and re-binding in templates makes things slower, so we don't want to do that
             '@typescript-eslint/unbound-method': 'off',
         }
     },
