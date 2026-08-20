9ff5a1055d75ab5502a075bf0004e24b2da62dc8
Christoph Reiter <reiter.christoph@gmail.com>
2026-08-20 11:45:01 +0200

    eslint: switch to recommendedTypeChecked with exceptions
    disable everything where there are findings, so we can enable them one by one

--- eslint-typescript.common.config.mjs
+++ eslint-typescript.common.config.mjs
@@ -1,7 +1,7 @@
 import tseslint from 'typescript-eslint';
 
 export default [
-    ...tseslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
     {
         languageOptions: {
             parserOptions: {
@@ -9,7 +9,20 @@ export default [
             }
         },
         rules:  {
+            '@typescript-eslint/await-thenable': 'off',
+            '@typescript-eslint/no-floating-promises': 'off',
+            '@typescript-eslint/no-misused-promises': 'off',
+            '@typescript-eslint/no-unsafe-argument': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-call': 'off',
+            '@typescript-eslint/no-unsafe-member-access': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
             '@typescript-eslint/no-unused-vars': ['error', {args: 'none'}],
+            '@typescript-eslint/only-throw-error': 'off',
+            '@typescript-eslint/prefer-promise-reject-errors': 'off',
+            '@typescript-eslint/require-await': 'off',
+            '@typescript-eslint/restrict-plus-operands': 'off',
+            '@typescript-eslint/unbound-method': 'off',
         }
     },
 ];
