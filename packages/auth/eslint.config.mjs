a604c6cbc0bd219a9e565c2d4770a86994a8c9a9
Christoph Reiter <reiter.christoph@gmail.com>
2026-08-20 11:29:46 +0200

    Add typescript-eslint to all other packages

--- packages/auth/eslint.config.mjs
+++ packages/auth/eslint.config.mjs
@@ -1,8 +1,10 @@
 import commonConfig from '../../eslint.common.config.mjs';
+import commonConfigTyped from '../../eslint-typescript.common.config.mjs';
 
 export default [
     {
-        ignores: ['dist/', '*.cjs'],
+        ignores: ['dist/', '*.cjs', 'test', '*.js'],
     },
     ...commonConfig,
+    ...commonConfigTyped,
 ];
--- toolkit-showcase/eslint.config.mjs
+++ toolkit-showcase/eslint.config.mjs
@@ -1,8 +1,10 @@
 import commonConfig from '../eslint.common.config.mjs';
+import commonConfigTyped from '../eslint-typescript.common.config.mjs';
 
 export default [
     {
-        ignores: ['dist/', '*.cjs'],
+        ignores: ['dist/', '*.js', 'test'],
     },
     ...commonConfig,
+    ...commonConfigTyped,
 ];
