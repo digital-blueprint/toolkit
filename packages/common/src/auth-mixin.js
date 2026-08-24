9b648551390a5d80ac04dfd6d397814c8a46064e
Christoph Reiter <reiter.christoph@gmail.com>
2026-08-24 15:30:17 +0200

    auth-mixin: add typing fixes

--- packages/common/src/auth-mixin.js
+++ packages/common/src/auth-mixin.js
@@ -1,70 +1,84 @@
 import {dedupeMixin} from '@open-wc/dedupe-mixin';
 
-export const AuthMixin = dedupeMixin(
-    (superClass) =>
-        class extends superClass {
-            static properties = {
-                auth: {type: Object},
-                .../** @type {{properties?: object}} */ (superClass).properties,
-            };
+/**
+ * @typedef {{auth: object | null, isAuthPending: () => boolean, isLoggedIn: () => boolean}} AuthMixinHost
+ * @typedef {import('@open-wc/dedupe-mixin').Constructor<import('lit').LitElement>} LitElementConstructor
+ * @typedef {import('@open-wc/dedupe-mixin').Constructor<AuthMixinHost>} AuthMixinHostConstructor
+ */
 
-            constructor() {
-                super();
-                this.auth = null;
-                this._previousAuthState = null;
-                this._authPending = true;
-            }
+/**
+ * @template {LitElementConstructor} T
+ * @param {T} superClass
+ * @returns {T & AuthMixinHostConstructor}
+ */
+const AuthMixinImplementation = (superClass) => {
+    const AuthMixinClass = class extends superClass {
+        static properties = {
+            auth: {type: Object},
+            .../** @type {{properties?: object}} */ (superClass).properties,
+        };
 
-            update(changedProps) {
-                if (changedProps.has('auth')) {
-                    const prevAuth = this._previousAuthState || {};
-                    const currentAuth = this.auth || {};
+        constructor(...args) {
+            super(...args);
+            this.auth = null;
+            this._previousAuthState = null;
+            this._authPending = true;
+        }
 
-                    const wasLoggedIn = prevAuth['login-status'] === 'logged-in';
-                    const wasLoggedOut = prevAuth['login-status'] === 'logged-out';
-                    const isLoggedIn = currentAuth['login-status'] === 'logged-in';
-                    const isLoggedOut = currentAuth['login-status'] === 'logged-out';
+        update(changedProps) {
+            if (changedProps.has('auth')) {
+                const prevAuth = this._previousAuthState || {};
+                const currentAuth = this.auth || {};
 
-                    if (!wasLoggedIn && isLoggedIn) {
-                        this._authPending = false;
-                        this.loginCallback(currentAuth);
-                    }
-                    if (!wasLoggedOut && isLoggedOut) {
-                        if (this._authPending) {
-                            this._authPending = false;
-                        } else {
-                            this.logoutCallback();
-                        }
-                    }
-                    this._previousAuthState = {...currentAuth};
+                const wasLoggedIn = prevAuth['login-status'] === 'logged-in';
+                const wasLoggedOut = prevAuth['login-status'] === 'logged-out';
+                const isLoggedIn = currentAuth['login-status'] === 'logged-in';
+                const isLoggedOut = currentAuth['login-status'] === 'logged-out';
+
+                if (!wasLoggedIn && isLoggedIn) {
+                    this._authPending = false;
+                    this.loginCallback(currentAuth);
                 }
-                super.update(changedProps);
+                if (!wasLoggedOut && isLoggedOut) {
+                    if (this._authPending) {
+                        this._authPending = false;
+                    } else {
+                        this.logoutCallback();
+                    }
+                }
+                this._previousAuthState = {...currentAuth};
             }
+            super.update(changedProps);
+        }
 
-            /**
-             * @returns {boolean} - True if the auth state is not settled yet.
-             */
-            isAuthPending() {
-                return this._authPending;
-            }
+        /**
+         * @returns {boolean} - True if the auth state is not settled yet.
+         */
+        isAuthPending() {
+            return this._authPending;
+        }
 
-            /**
-             * @returns {boolean} - True if the user is logged in, false otherwise.
-             */
-            isLoggedIn() {
-                return this.auth && this.auth['login-status'] === 'logged-in';
-            }
+        /**
+         * @returns {boolean} - True if the user is logged in, false otherwise.
+         */
+        isLoggedIn() {
+            return this.auth && this.auth['login-status'] === 'logged-in';
+        }
 
-            /**
-             * Called when user logs in, or on load when the user is logged in.
-             * @param {object} auth - The auth state with login information, same as this.auth
-             */
-            loginCallback(auth) {}
+        /**
+         * Called when user logs in, or on load when the user is logged in.
+         * @param {object} auth - The auth state with login information, same as this.auth
+         */
+        loginCallback(auth) {}
 
-            /**
-             * Called when user logs out or was logged out. Only gets called if loginCallback()
-             * was called before.
-             */
-            logoutCallback() {}
-        },
-);
+        /**
+         * Called when user logs out or was logged out. Only gets called if loginCallback()
+         * was called before.
+         */
+        logoutCallback() {}
+    };
+
+    return AuthMixinClass;
+};
+
+export const AuthMixin = dedupeMixin(AuthMixinImplementation);
